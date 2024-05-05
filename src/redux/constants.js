7d5a9d659b7dc10fc33039064c6ed48cf9cb35fa
MSvidlo <143841475+MSvidlo@users.noreply.github.com>
2024-05-06 00:00:23 +0200

    final changes

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,5 +1,5 @@
 export const CONSTANTS = {
-    CONTACTS_INIT: [{ id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+    CONTACTS_INIT: [{ id: "id-1", name: "Anita Gilson", number: "456-14-56" },
     { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
     { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
